01d3d846891fab9365fec72d4180c443bd5e0bd5
Kevin Beach <kevin.beach@macnica.com>
2026-02-08 11:44:33 +0000

    initial commit, same as r4

---
+++ src/version_info.h
@@ -0,0 +1,16 @@
+/*
+ * version_info.h
+ *
+ *  Created on: 28 Jan 2026
+ *      Author: Kevin Beach
+ */
+
+#ifndef VERSION_INFO_H_
+#define VERSION_INFO_H_
+
+#define VERSION_NO "1.2.1"
+#define VERSION_RESOLUTION "1920 x 1080"
+#define VERSION_INFO "R4_1 FPGA (UVC rev B) FPGA incl gamma. Added vertical binning camera_initialization.h changed  "
+#define VERSION_GITHUB "Not yet pushed"
+
+#endif /* VERSION_INFO_H_ */
