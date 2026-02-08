b099c9acf2aa598aac4143e9f04639712b8c534c
Kevin Beach <kevin.beach@macnica.com>
2026-02-08 12:24:34 +0000

    Added in verbose mode. Version 1.2.1

--- src/version_info.h
+++ src/version_info.h
@@ -10,7 +10,7 @@
 
 #define VERSION_NO "1.2.1"
 #define VERSION_RESOLUTION "1920 x 1080"
-#define VERSION_INFO "R4_1 FPGA (UVC rev B) FPGA incl gamma. Added vertical binning camera_initialization.h changed  "
-#define VERSION_GITHUB "Not yet pushed"
+#define VERSION_INFO "R4_1 FPGA (UVC rev B) FPGA incl gamma. Added in Verbose Mode options for Python script"
+#define VERSION_GITHUB "https://github.com/KevinBeach/r4_1_crosslinku_demo/tree/master"
 
 #endif /* VERSION_INFO_H_ */
